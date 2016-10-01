968eaeb92c2f7c10cfbd9767dc0d4bbc3d56c52c
Chris Cohoat <chris.cohoat@gmail.com>
2016-10-01 12:37:41 -0400

    Updated run script

--- python/scripts/run.sh
+++ python/scripts/run.sh
@@ -1,1 +1,4 @@
-docker run -it -p 9999:9999 --privileged -d -v /dev:/dev chriscohoat/aquaponics
+docker build -t chriscohoat/aquaponics .
+docker rm -v --force `docker ps -qa`
+docker run -it -p 9999:9999 --name aquaponics --privileged -d -v /dev:/dev chriscohoat/aquaponics
+docker logs -f aquaponics
